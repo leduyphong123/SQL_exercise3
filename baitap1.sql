bac0fe4926f8227d4f45489ca2ac293a910e28fb
leduyphong123 <leduyphongck789@gmail.com>
2023-06-23 10:18:04 +0700

    baitap1

---
+++ baitap1.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE QUANLYSINHVIEN;
+USE QUANLYSINHVIEN;
+CREATE TABLE CLASS(
+	ClassID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ClassName VARCHAR(60),
+    StartDate DATETIME NOT NULL,
+    Status BIT
+);
+
+CREATE TABLE STUDENT(
+	StudentID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    StudentName VARCHAR(30) NOT NULL,
+    Address VARCHAR(50),
+    Phone VARCHAR(20),
+    Status BIT,
+    ClassID INT NOT NULL,
+    FOREIGN KEY (ClassID) REFERENCES CLASS(ClassID)
+);
+
+CREATE TABLE SUBJECT_SCHOOL(
+	SubID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SubName VARCHAR(30) NOT NULL,
+    Credit TINYINT NOT NULL DEFAULT 1,
+    Status BIT DEFAULT 1,
+    CHECK (CREDIT >=1)
+);
+
+CREATE TABLE MARK(
+	MarkID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SubId INT,
+    StudentID INT,
+    Mark FLOAT DEFAULT 0 CHECK (MARK BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubID,StudentID),
+    FOREIGN KEY (SubID) REFERENCES SUBJECT_SCHOOL (SubID),
+    FOREIGN KEY (StudentID) REFERENCES STUDENT (StudentID)
+);
+
+USE QUANLYSINHVIEN;
+INSERT INTO Class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO Class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO Class
+VALUES (3, 'B3', current_date, 0);
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO Student (StudentName, Address, Status, ClassId)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+INSERT INTO Subject_SCHOOL
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
